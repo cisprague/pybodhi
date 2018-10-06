a0a06ebf84c50b74969947e9de536af6ffe54519
cisprague <christopher.iliffe.sprague@gmail.com>
2018-10-06 10:01:53 +0200

    Updated setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+# Christopher Iliffe Sprague
+# christopher.iliffe.sprague@gmail.com
+
+from distutils.core import setup
+
+setup(
+    name='pybodhi',
+    version='0.1',
+    packages=['pybodhi'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
